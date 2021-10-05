eb1c773194f4136714f6f5f5ffcb924709e721f7
IhorBakaisa <73829374+IhorBakaisa@users.noreply.github.com>
2021-10-05 12:37:42 +0300

    test

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <student v-for="student in studentList" :key="student.id"
+    :photoStudent="student.photoStudent"
+    :nameStudent="student.nameStudent"
+    :studentAddress="student.studentAddress"
+    />
+  </div>
+</template>
+
+<script>
+import Student from './components/Student'
+
+export default {
+  name: 'App',
+  components: {
+    Student
+  },
+
+  data() {
+    return {
+      studentList: [
+        {
+          photoStudent: "",
+          nameStudent: "Ігор",
+          studentAddress: "Ужгород"
+        },
+        {
+          photoStudent: "",
+          nameStudent: "Євген",
+          studentAddress: "Чоп"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
