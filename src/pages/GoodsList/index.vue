a40f79b9faee613d18048aca7a13c57ed47bd5cd
DanyaTRPC <73814062+DanyaTRPC@users.noreply.github.com>
2021-10-26 19:47:36 +0300

    Add files via upload

--- src/pages/GoodsList/index.vue
+++ src/pages/GoodsList/index.vue
@@ -1,11 +1,51 @@
 <template>
     <div>
- <p> Вибрати товар за<br> <select size="1" class="select">
-<option value="1">ціною</option>
-<option value="2">датою </option>
-</select></p>
-   <p><input type="submit" value="Відсортувати" class="butsort"></p>
-  
+        <div class="sort">
+            <p> Вибрати товар за <select size="1" class="select">
+            <option value="1">ціною</option>
+            <option value="2">датою </option>
+            </select>
+            <input type="submit" value="Відсортувати" class="butsort"></p>
+        </div>
+        <div class="categories">
+            <p id="catg">Категорії</p>
+            <button class="btnCategories">
+                Реферати
+            </button>
+            <br>
+            <br>
+            <button class="btnCategories">
+                Конспекти
+            </button>
+            <br>
+            <br>
+            <button class="btnCategories">
+                Лабораторні
+            </button>
+            <br>
+            <br>
+            <button class="btnCategories">
+                Самостійні
+            </button>
+            <br>
+            <br>
+            <button class="btnCategories">
+                Контрольні
+            </button>
+            <br>
+            <br>
+            <button class="btnCategories">
+                Індивідуальні
+            </button>
+            <br>
+            <br>
+            <button class="btnCategories">
+                Курсові
+            </button>
+        </div>
+        <div class="mainList">
+            dghdgd
+        </div>
     </div>
 </template>
 
@@ -16,6 +56,39 @@
 </script>
 
 <style >
+.mainList{
+    border: 2px solid;
+    margin-left: 210px;
+    background-color: black;
+    margin-top: -465px;
+    height: 800px;
+    width: 1450px;
+}
+.btnCategories{
+    height: 50px;
+    width: 190px;
+    padding-top: 10px ;
+    border-radius: 4px;
+    background-color: Gold;
+
+
+    color: Black;
+    text-decoration: none;
+}
+
+.btnCategories:hover {
+  background-color: DarkOrange; 
+  color: white;
+}
+.categories{
+    margin-top: -58px;
+    color: gold;
+    border: 2px solid;
+    width: 200px;
+    height: 525px;
+    background-color: black;
+    margin-left: -10px;
+}
 .butsort{
   border-radius: 4px;
   border-color: black;
@@ -28,5 +101,11 @@
 }
 .select{
     background: LemonChiffon;
+}
+.sort{
+    border: 2px solid;
+    width: 1450px;
+    margin-left: 210px;
+    background-color: black;
 }
 </style>
