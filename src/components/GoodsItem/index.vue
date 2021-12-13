4a6ff1ed6a068b519af5f0b54195032d156d9f47
DanyaTRPC <73814062+DanyaTRPC@users.noreply.github.com>
2021-12-13 21:11:50 +0200

    Add files via upload

--- src/components/GoodsItem/index.vue
+++ src/components/GoodsItem/index.vue
@@ -4,7 +4,7 @@
             <div class="cardPhoto"><img :src="goods.photo" alt="photo" class="photo"></div> <br> 
             <div class="cardInfo">{{goods.name}}</div> <br>
             <div class="cardInfo">{{goods.price}} грн</div>
-            <button>Переглянути</button>
+            <router-link to="/AboutGoods" class="pages View">Переглянути</router-link>
         </div>
     </div>
 </template>
@@ -21,57 +21,58 @@
     }
 </script>
 
-<style lang="css" scoped>
 
-.blockgoods {
-    width: 250px;
-    border: 4px solid rgb(0, 0, 0);
-    margin: 3px;
-    border-radius:  20px;
-    border-color:gold ;
-    color: black;
-    background: white;
-}
-
-.photo {
-    width: 200px;
-    height: 200px;
-}
-
-.cardPhoto {
-    margin-top: 20px;
-    margin-left: 5px;
-    height: 200px;
-}
-
-.cardInfo {
-    border: 2px solid black;
-    color: Gold;
-    border-color: gold;
-    background: black;
-    margin-left: 40px;
-    margin-right: 40px;
-    border-radius: 5px;
-}
-
-button {
-  border-color: gold;
-  color: #fff;
-  box-shadow: 0 0 0 0 black;
-  -webkit-transition: all 150ms ease-in-out;
-  transition: all 150ms ease-in-out;
-  border-radius: 6px;
-  background-color: black;
-  margin-left: 0px;
-  padding: 10px;
-  color: gold;
-  text-decoration: none;
-  margin-top: 25px;
-  border: 2px solid gold;
-  width: 175px;
-}
-button:hover {
-  box-shadow: 0 0 10px 0 gold inset, 0 0 10px 4px gold;
-}
-
+<style lang="css" scoped> 
+ 
+.blockgoods { 
+    width: 250px; 
+    border: 4px solid rgb(0, 0, 0); 
+    margin: 3px; 
+    border-radius:  20px; 
+    border-color:gold ; 
+    color: black; 
+    background: white; 
+} 
+ 
+.photo { 
+    width: 200px; 
+    height: 200px; 
+} 
+ 
+.cardPhoto { 
+    margin-top: 20px; 
+    margin-left: 5px; 
+    height: 200px; 
+} 
+ 
+.cardInfo { 
+    border: 2px solid black; 
+    color: Gold; 
+    border-color: gold; 
+    background: black; 
+    margin-left: 40px; 
+    margin-right: 40px; 
+    border-radius: 5px; 
+} 
+ 
+button { 
+  border-color: gold; 
+  color: #fff; 
+  box-shadow: 0 0 0 0 black; 
+  -webkit-transition: all 150ms ease-in-out; 
+  transition: all 150ms ease-in-out; 
+  border-radius: 6px; 
+  background-color: black; 
+  margin-left: 0px; 
+  padding: 10px; 
+  color: gold; 
+  text-decoration: none; 
+  margin-top: 25px; 
+  border: 2px solid gold; 
+  width: 175px; 
+} 
+button:hover { 
+  box-shadow: 0 0 10px 0 gold inset, 0 0 10px 4px gold; 
+} 
+ 
 </style>
--- src/pages/AboutGoods/index.vue
+++ src/pages/AboutGoods/index.vue
@@ -1,6 +1,24 @@
 <template>
-    <div>
-        AboutGoods
+    <div class="blockAboutGoods">
+        <div class="photoAboutGoods">
+            Фото
+        </div>
+        <p id="infoAboutGoods">
+            Ціна: 1488$
+            <br>
+            <br>
+            Прізвища Ім’я Побатькові 
+            <br>
+            <br> 
+            Контакти 
+            <br>
+            <br>
+            Навчальний заклад
+        </p>
+        <h2 id="nameGoods">Назва товару</h2>
+        <div class="description">
+            Опис...
+        </div>
     </div>
 </template>
 
