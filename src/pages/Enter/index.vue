0b7f572290ea18d66c2d15c0d4bea01d3852d9c9
ProntekerDaniela <73825709+ProntekerDaniela@users.noreply.github.com>
2021-12-13 18:40:39 +0300

    Add files via upload

--- src/pages/Enter/index.vue
+++ src/pages/Enter/index.vue
@@ -1,17 +1,21 @@
 <template>
+<form @submit.prevent="submitHandler">
     <div>
         <LoginIcon class="icon4"/>
         <div class="blockEnt">
-            <h3 >Вхід</h3>
-            <input type="text" placeholder="Логін">
-            <br>
-            <br>
-            <input type="text" placeholder="Пароль">
-            <br>
-            <br>
-            <button>Увійти</button>
+        <h3 >Вхід</h3>
+      <input type="email" placeholder="Пошта"  v-model="email" />
+      <br>
+      <br>
+      <input type="password" placeholder="Пароль"  v-model="password" />
+      <br>
+      <br>
+      <loader class="loader" v-if="loading" />
+      <p>{{errorMessage}}</p>
+       <button>Увійти</button>
         </div>
     </div>
+     </form>
 </template>
 
 <script>
@@ -19,7 +23,35 @@ import { LoginIcon } from "@vue-hero-icons/outline"
     export default {
         name: 'Enter',
 components: { LoginIcon
-  }
+  },
+    data() {
+    return {
+      email: "",
+      password: "",
+      errorMessage: ""
+    };
+  },
+  methods: {
+    async submitHandler() {
+      if (!this.email || !this.password) {
+        this.errorMessage = "Усі поля мають бути заповнені";
+        return;
+      }
+      if (this.password.length < 6) {
+        this.errorMessage = "Довжина пароля має бути більше 6 символів";
+        return;
+      }
+      try {
+        await this.$store.dispatch("Enter", {
+          email: this.email,
+          password: this.password
+        });
+        this.$router.push("/MyAccount");
+      } catch (error) {
+        this.errorMessage = this.error;
+      }
     }
+  }
+};
 </script>
 
--- src/pages/Registration/index.vue
+++ src/pages/Registration/index.vue
@@ -1,29 +1,27 @@
 <template>
+ <form  @submit.prevent="submitHandler">
     <div>
         <UserAddIcon class="icon5"/>
         <div class="blockRegestration">
             <h3 >Реєстрація</h3>
-            <input type="text" placeholder="П.І.Б">
-            <br>
-            <br>
-            <input type="text" placeholder="Пошта">
-            <br>
-            <br>
-            <input type="text" placeholder="Номер">
-            <br>
-            <br>
-            <input type="text" placeholder="Логін">
-            <br>
-            <br>
-            <input type="text" placeholder="Пароль">
-            <br>
-            <br>
-            <input type="text" placeholder="Повтор паролю">
-            <br>
-            <br>
-            <button>Зареєструватися</button>
+            <input type="email" placeholder="Пошта"  v-model="email" />
+      <br>
+      <br>
+      <input type="text" placeholder="П.І.Б"  v-model="name" />
+      <br>
+      <br>
+      <input type="password" placeholder="Пароль"  v-model="password" />
+      <br>
+      <br>
+      <input type="password" placeholder="Повтор паролю" v-model="confirmPassword"/>
+      <br>
+      <br>
+      <loader class="loader" v-if="loading" />
+      <p>{{errorMessage}}</p>
+       <button>Зареєструватися</button>
         </div>
     </div>
+     </form>
 </template>
 
 <script>
@@ -31,6 +29,44 @@ import { UserAddIcon } from "@vue-hero-icons/outline"
     export default {
         name: 'Registration',
  components: { UserAddIcon
-  }
+  },
+    data() {
+    return {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      name: "",
+      errorMessage: ""
+    };
+  },
+  methods: {
+    async submitHandler() {
+      if (
+        !this.email ||
+        !this.name ||
+        !this.password ||
+        !this.confirmPassword
+      ) {
+        this.errorMessage = "Усі поля мають бути заповнені";
+        return;
+      }
+      if (this.password !== this.confirmPassword) {
+        this.errorMessage = "Паролі не співпадають";
+        return;
+      }
+      try {
+        await this.$store.dispatch("Registration", {
+          email: this.email,
+          password: this.password,
+          name: this.name
+        });
+        this.$router.push("/MyAccount");
+      } catch (error) {
+        console.log(this.error);
+        this.errorMessage = this.error;
+        console.log(this.errorMessage);
+      }
     }
+  }
+};
 </script>
